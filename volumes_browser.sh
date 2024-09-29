27e039585f7dccc3433b696daa62a787f05f8418
Eric Duminil <eric.duminil@gmail.com>
2024-09-29 17:13:17 +0200

    TODO

--- volumes_browser.sh
+++ volumes_browser.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-MODE=ro
-VOLUMES=.
+MODE=ro # TODO: add to params
+VOLUMES=. # TODO: add to params
 
 green=$(tput setaf 2)
 orange=$(tput setaf 9)
@@ -12,7 +12,7 @@ no_color=$(tput sgr0)
 
 COLOR=$green
 mount=""
-IMAGE="busybox:latest"
+IMAGE="busybox:latest" # TODO: add to params
 
 for VOLUME_NAME in $(docker volume ls --format "{{.Name}}" | grep ${VOLUMES}); do
   echo "Mount ${COLOR}${VOLUME_NAME}${no_color} to /mnt/${VOLUME_NAME}";
